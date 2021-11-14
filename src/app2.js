6b241235093b9402600420f22283cf02ee0f478d
TIMPICKLE <NIHAODONG1621@gmail.com>
2021-11-14 23:07:41 +0800

    v1

--- src/app2.js
+++ src/app2.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, {useEffect} from "react";
 import './App.css';
 import { Rnd } from "react-rnd";
 
@@ -6,25 +6,9 @@ import { Rnd } from "react-rnd";
 import Button from "@mui/material/Button";
 
 function App() {
-    let [w,setW ]= React.useState(300 )
-    let [h,setH] =React.useState(450)
-    let myRef =React.createRef()
+    const [w,setW ]= React.useState(300 )
+    const [h,setH] =React.useState(450)
 
-    let play = (width,height) =>{
-        let v = document.getElementById("video1");
-        let c = document.getElementById("myCanvas");
-        let ctx = c.getContext('2d');
-        v.play()//需求只说了 播放
-        //每10毫秒画一次图
-        v.addEventListener('play', function() {
-            let i = window.setInterval(function() {
-                ctx.drawImage(v, 1, 1, width, height);
-                if(v.ended){
-                    clearInterval(i)
-                }
-            }, 10);
-        }, false);
-    }
 
     return (
         <div className="App" id='app'>
@@ -41,29 +25,29 @@ function App() {
                  minHeight={212}
                  bounds="window"
                  onResizeStop={(e, direction, ref, delta, position) => {
+                     // this.setState({
+                     //     h: ref.offsetHeight,
+                     //     w: ref.offsetWidth
+                     // });
                      setH(ref.offsetHeight)
                      setW(ref.offsetWidth)
-                     play(ref.offsetWidth,ref.offsetHeight)
+
                  }}
-            >  <div >
-                <MyCanvas ref={myRef} w={w} h={h} className='fcan'>
+            >
+                <MyCanvas w={w} h={h} className='fcan'>
                 </MyCanvas>
-            </div>
+
             </Rnd>
-
-
-
-
         </div>
     );
 }
 
 
 function MyCanvas(props) {
-
+    var www = props.w
+    var hhh = props.h
+    console.log(www)
     let play = (width,height) =>{
-        width=props.w;
-        height=props.h;
         let v = document.getElementById("video1");
         let c = document.getElementById("myCanvas");
         let ctx = c.getContext('2d');
@@ -71,7 +55,7 @@ function MyCanvas(props) {
         //每10毫秒画一次图
         v.addEventListener('play', function() {
             let i = window.setInterval(function() {
-                ctx.drawImage(v, 1, 1, width, height);
+                ctx.drawImage(v, 1, 1, www, hhh);
                 if(v.ended){
                     clearInterval(i)
                 }
@@ -83,7 +67,7 @@ function MyCanvas(props) {
         <div className='son' width={props.w} height={props.h}>
             <video style={{display: 'none'}}   crossOrigin="anonymous" id='video1'
                    src="https://mdn.github.io/dom-examples/canvas/chroma-keying/media/video.mp4"></video>
-            <canvas width='800'  height='600'    id='myCanvas'>
+            <canvas style={{width: props.w, height: props.h}}    id='myCanvas'>
             </canvas>
             <br></br>
             <Button onClick={play}>
@@ -97,4 +81,4 @@ function MyCanvas(props) {
 
 
 
-export default App;
+export default MyCanvas;
